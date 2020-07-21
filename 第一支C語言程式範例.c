c25b3c2ec0b7d32864f88abb8b37cc99a82842aa
LiJieAn <68594007+LiJieAn@users.noreply.github.com>
2020-07-21 19:40:07 +0800

    Add files via upload

---
+++ 第一支C語言程式範例.c
@@ -0,0 +1,20 @@
+/*
+程式檔案名稱:OMessage.c
+作者:康廷數位
+建立時間:2018/12/30
+程式功能說明
+第一支c語言示範說明程式
+在畫面上輸出一條預先指定的文字訊息
+*/
+#include<stdio.h>
+#include<stdlib.h>
+/*程式進入點main*/
+int main()
+{
+	int x; /*宣告整數變數*/
+	x= 100;/**/
+	printf("x:%d \n",x);/*輸出訊息*/
+	system("pause");/*暫停*/
+	return 0;/*回傳運算結果*/
+	
+ } 
