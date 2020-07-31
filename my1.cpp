44ad52e072584eb5e080f42e3042b01904f2d897
LiJieAn <68594007+LiJieAn@users.noreply.github.com>
2020-07-31 19:08:04 +0800

    Add files via upload

---
+++ my1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Hello world";/*cout<<=computer out¹q¸£¿é¥X*/
+	return 0;
+}
---
+++ my2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>/*用來載入輸入輸出功能*/
+using namespace std;
+
+int main()
+{
+	//c++有兩種註解寫法:// /**/
+	// robot program starts here!!
+	/*You are simulating Sophia robot.*/
+	int age;/*宣告變數age是一個整數的變數型態*/
+	cout<<"Hello will Smith.\n";/*\n=換行*/
+	cout<<"My name is Sophia.\n";
+	cout<<"Nice to meet you.\n";
+	cout<<"I am 32 years old, and you?\n";
+	cin>>age;/*cin=電腦輸入*/
+	cout<<"Great!I know your age is "<<age<<" years old.";
+	return 0;
+}
+
