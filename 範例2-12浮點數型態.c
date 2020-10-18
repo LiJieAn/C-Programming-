07dd9454aa8198355b8acd43bbb142f6109dddd5
LiJieAn <68594007+LiJieAn@users.noreply.github.com>
2020-10-18 16:23:36 +0800

    Add files via upload

---
+++ 範例2-12浮點數型態.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+
+int main()
+{
+	float f=123.45678f;/*此行的輸出不正確 因為float精確度無法完整輸出到8位數*/
+	double d=123.45678;
+	printf("float 變數 s=%f \n",f);
+	printf("double 變數 s=%f \n",d);
+	return 0;
+}
