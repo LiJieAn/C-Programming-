956d2aa47db0a73da65fabb8ca6b5c580b842929
LiJieAn <68594007+LiJieAn@users.noreply.github.com>
2020-11-29 10:54:56 +0800

    Add files via upload

---
+++ 範例2-15以整數格式輸出.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	char  c1='A';
+	char  c2='a';
+	printf("A的字元編碼=%d \n",c1);
+	printf("a的字元編碼=%d \n",c2);
+	return 0;
+	
+ } 
+
+
+
+
+/*可指定%d來輸出特定字元所對應的編碼值
+ex:
+char x ='A';
+printf("%d",x);
+→得到65的輸出結果*/ 
+/*%c：以字元方式輸出
+%d：10 進位整數輸出*/ 
+/*char	字元(半形字)	Character	'a'、'R'、'1'、'@'、'*' ...*/
