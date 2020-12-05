f109174daeb682e5a55cf29c14c6169a1f7a7dbf
LiJieAn <68594007+LiJieAn@users.noreply.github.com>
2020-12-05 11:11:22 +0800

    Add files via upload

---
+++ 範例3-08變數自身設值運算.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int x=100;
+	int y=200;
+	int z=300;
+	
+	x=x+100;
+	y=y+x;
+	z=z+y;
+	printf("x等於%d \n",x);
+	printf("y等於%d \n",y);
+	printf("z等於%d \n",z);
+	return 0;
+ } 
+ 
