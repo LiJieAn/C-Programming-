a2d5679d02ab414ac23d594538a5824aeefe58cd
LiJieAn <68594007+LiJieAn@users.noreply.github.com>
2020-12-06 01:20:58 +0800

    Add files via upload

---
+++ 範例4-01示範printf().c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello,C! \n");
+	return 0;
+ } 
+/*printf("輸出格式字串",arg1,arg2,...);
+輸出格式字串為必要 以雙引號包裹標示
+引數arg1 arg2為選擇性的 可是變數 常數 一段運算式
+printf()函數主要功能 提供自訂格式內容的輸出支援*/
---
+++ 範例4-03輸出變數與{運算式結果}.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int a=123;
+	int b=200;
+	int c=300;
+	printf("%d+%d+%d=%d \n",a,b,c,a+b+c);
+	return 0;
+ }
+ /*可直接在printf內進行數學運算*/ 
