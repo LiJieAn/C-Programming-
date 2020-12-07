247e27c17ac17980d1a066ee3a82e84cbeaeb559
LiJieAn <68594007+LiJieAn@users.noreply.github.com>
2020-12-07 21:19:16 +0800

    Add files via upload

---
+++ 範例4-08小點位數.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	double a=10023.456789;
+	printf("%.3f \n",a);/*挡狦 10023.456计翴よΤ计砆耝ㄓ*/ 
+	return 0;
+
+}
+
+/*皐癸计翴 钡﹚计计
+ %.3△ボ璶计翴よ计
+ 
+  %[耿迭][块糴].[计计]Αじ*/ 
