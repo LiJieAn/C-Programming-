acffc27309aca32284081c22257635934f1e9077
LiJieAn <68594007+LiJieAn@users.noreply.github.com>
2020-12-15 00:03:10 +0800

    Add files via upload

---
+++ 範例6-01重複執行程式.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int i=0;
+	i+=1;
+	i+=2;
+	i+=3;
+	i+=4;
+	i+=5;
+	i+=6;
+	i+=7;
+	i+=8;
+	i+=9;
+	i+=10;
+	printf("1加到10等於%d\n",i);
+	return 0;
+
+	
+}
+ 
+
---
+++ 範例6-03無窮迴圈for(;;).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int i=0;
+	for(;;)/*沒有任何運算式的for迴圈*/ 
+	{
+		i++;
+		printf("loop:%d \n",i); 
+		if(i>10) breal;/*break如同在switch的用法 強制中止此迴圈的執行作業*/ 
+	}
+	return 0;
+	
+ } 
+ /*此迴圈一旦執行就不會中止 但是他是一個成功的程式*/
+ /*當沒有辦法明確指定何時停止程式的執行→在for迴圈中加入if判斷式*/ 
+  
