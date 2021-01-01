956f1eb4542c824b4a98556961acda705e2c7684
LiJieAn <68594007+LiJieAn@users.noreply.github.com>
2021-01-01 19:53:11 +0800

    Add files via upload

---
+++ 範例9-02陣列的變數索引存取.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int x[6];
+	int i=3,j=4,k=5;
+	x[0]=100;
+	x[1]=200;
+	x[2]=300;
+	x[3]=400;
+	x[4]=500;
+	x[5]=600;
+	printf("%d,%d,%d,%d,%d,%d \n",x[0],x[1],x[2],x[k],x[j],x[i]);
+	return 0;
+	
+}
